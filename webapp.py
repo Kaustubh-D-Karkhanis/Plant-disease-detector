8066fc88334834b5fb5a80ca10971baabdb1428e
Himanshu Bharuka <84191694+bharukahimanshu@users.noreply.github.com>
2023-05-03 12:01:19 +0530

    UI changes

--- webapp.py
+++ webapp.py
@@ -14,13 +14,13 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html = True)
 
-st.title('Maize Leaf Disease Prediction')
+st.title('Maize & Potato Leaf Disease Prediction')
 
 def main() :
-    file_uploaded = st.file_uploader('Choose an image...', type = 'jpg')
+    file_uploaded = st.file_uploader('Choose an image to predict...', type = 'jpg')
     if file_uploaded is not None :
         image = Image.open(file_uploaded)
-        st.write("Uploaded Image.")
+        st.write("Uploaded Image..")
         figure = plt.figure()
         plt.imshow(image)
         plt.axis('off')
